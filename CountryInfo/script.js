a5b988e1e3de19488c7d929787a5b512ddc354cb
tharunkumar7097 <119399188+tharunkumar7097@users.noreply.github.com>
2023-09-13 11:13:02 +0530

    script.js

--- CountryInfo/script.js
+++ CountryInfo/script.js
@@ -1,1 +1,22 @@
+const API_URL = 'https://restcountries.com/name/';
 
+const getCountryData = async (countryName) => {
+  const response = await fetch(`${API_URL}${countryName}`);
+  const data = await response.json();
+  return data;
+};
+
+const renderCountryInfo = (data) => {
+  const countryInfoElement = document.querySelector('.country-info');
+  countryInfoElement.innerHTML = `
+    <h3>${data.name}</h3>
+    <h4>${data.region}</h4>
+    <p>Population: ${data.population}</p>
+    <p>Languages: ${data.languages}</p>
+    <p>Currencies: ${data.currencies}</p>
+  `;
+};
+
+const countryName = prompt('Enter the name of a country:');
+
+getCountryData(countryName).then(renderCountryInfo);
